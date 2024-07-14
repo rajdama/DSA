56c7824b3e8d48b5aae7fbc0d434a189c5cf4125
Raj <75741866+rajdama@users.noreply.github.com>
2024-07-14 12:08:33 +0530

    Time: 35 ms (5.22%) | Memory: 42.4 MB (43.37%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -5,7 +5,6 @@ class Solution {
         if(s.length()!=t.length()){
             return false;
         }
-        int k=1;
         for(int i=0;i<s.length();i++){
              if(map1.containsKey(s.charAt(i)) && map1.get(s.charAt(i)) !=t.charAt(i) || map2.containsKey(t.charAt(i)) && map2.get(t.charAt(i)) !=s.charAt(i)){
                return false;
