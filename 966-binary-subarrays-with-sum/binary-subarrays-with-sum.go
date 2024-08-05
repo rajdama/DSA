80f6e18bf10326c8f8948c6f2733be525bd0e8b3
Raj <75741866+rajdama@users.noreply.github.com>
2024-08-05 14:08:01 +0530

    Time: 37 ms (30.40%) | Memory: 6.6 MB (81.60%) - LeetSync

---
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.go
@@ -0,0 +1,23 @@
+func lessThanEquals(a []int, k int) int {
+    if k < 0 {
+        return 0
+    }
+    sum := 0
+    count := 0
+    left := 0
+    right := 0
+    for right < len(a) {
+        sum += a[right]
+        for sum > k {
+            sum -= a[left]
+            left++
+        }
+        count += right - left + 1
+        right++
+    }
+    return count
+}
+
+func numSubarraysWithSum(nums []int, goal int) int {
+    return lessThanEquals(nums, goal) - lessThanEquals(nums, goal-1)
+}
